3ea733ca06ae5b1c34d3bb00e51a9cd8ade9b0e9
neverbirth <neverbirth@gmail.com>
2012-06-14 08:23:42 +0000

    New setting default filter. Unhooked. Made project id an integer. Set settings categories.

--- AnkhMantisConnector/IssueTracker/ConnectorSettings.cs
+++ AnkhMantisConnector/IssueTracker/ConnectorSettings.cs
@@ -19,48 +19,65 @@ namespace AnkhMantisConnector.IssueTracker
         private const string DefaultCloseCommitNoteText = "Issue closed in Revision {0}:\n{1}";
 
         [Description("The Id of the project to track")]
-        public string ProjectId { get; set; }
+        [Category("Issues View")]
+        public int ProjectId { get; set; }
 
         [Description("Address to the root path of the MantisBT installation")]
+        [Category("Connection")]
         public Uri RepositoryUri { get; set; }
 
         [Description("User name used to connect to the tracker")]
+        [Category("Connection")]
         public string UserName { get; set; }
 
         [Description("Password used to connect to the tracker")]
+        [Category("Connection")]
         public string Password { get; set; }
 
         [DefaultValue(50)]
         [Description("Number of issues to fetch per page")]
+        [Category("Issues View")]
         public int IssuesPerPage { get; set; }
 
         [DefaultValue(false)]
         [Description("Determines whether the user and password data should be stored in a local file instead of using a SVN property")]
+        [Category("Connection")]
         public bool LocalAccount { get; set; }
 
         [DefaultValue(DefaultWebServicePath)]
         [Description("The relative path to the SOAP service")]
+        [Category("Connection")]
         public string WebServicePath { get; set; }
 
         [DefaultValue(0)]
         [Description("The method used to retrieve data from the tracker")]
+        [Category("Connection")]
         public FetchingMethod FetchingMethod { get; set; }
 
         [DefaultValue(DefaultIssuePattern)]
         [Description("The pattern used to match the issue ids present in a commit message")]
+        [Category("Integration")]
         public string IssuePattern { get; set; }
 
         [DefaultValue(true)]
         [Description("Whether a note should be added to the issue in the tracker after a commit")]
+        [Category("Integration")]
         public bool AddNoteAfterCommit { get; set; }
 
         [DefaultValue(DefaultAssociatedCommitNoteText)]
         [Description("The text for the note to add to each associated issue in a commit")]
+        [Category("Integration")]
         public string AssociatedCommitNoteText { get; set; }
 
         [DefaultValue(DefaultCloseCommitNoteText)]
         [Description("The text for the note to add to each closed issue in a commit")]
+        [Category("Integration")]
         public string CloseCommitNoteText { get; set; }
+
+        [DefaultValue(-1)]
+        [Description("Default filter to use when viewing issues. Use -1 in order to display all issues")]
+        [Category("Issues View")]
+        public int DefaultFilter { get; set; }
 
         public ConnectorSettings()
         {
@@ -70,6 +87,7 @@ namespace AnkhMantisConnector.IssueTracker
             AddNoteAfterCommit = true;
             CloseCommitNoteText = DefaultCloseCommitNoteText;
             AssociatedCommitNoteText = DefaultAssociatedCommitNoteText;
+            DefaultFilter = -1;
         }
 
     }
